80413ab1bf69df1357a5706d3396d12b79222562
morrisq <morrisq111@gmail.com>
2019-11-27 23:03:38 -0500

    Created a way to filter out csv files and datasets so we can take the appropriate variables. Downloading and creating the csv files necessary to analyze the data is up to you.

---
+++ Tidy Data.R
@@ -0,0 +1,24 @@
+library(dplyr)
+
+###Tidying up the data
+
+kickstarter <- read.csv("./kickstarter_data_with_features.csv")
+
+#filter out based on kickstarters with US currency ONLY
+us_kickstarters <- kickstarter %>%
+  filter(kickstarter$currency == "USD")
+
+#Filter based on kickstarters without US currency
+non_us_kickstarters <- kickstarter %>%
+  filter(kickstarter$currency != "USD")
+
+#Filter based on successful kickstarters
+successful_kickstarters <- kickstarter %>%
+  filter(kickstarter$state == "successful")
+
+#Filter full dataset down to the columns for goal, pledged, state, number of backers, and launch date
+#The units for goal and pledged are in US dollars and properly converted based on the static_usd_rate column
+reduced_dataset <- kickstarter %>%
+  select(goal, pledged, state, static_usd_rate, backers_count, launched_at_weekday) %>%
+  mutate(goal = goal*static_usd_rate) %>%
+  mutate(pledged = pledged*static_usd_rate)
