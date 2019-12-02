bfb13cc228d533a95a8f16c04cdc4948e1fa1552
morrisq <morrisq111@gmail.com>
2019-12-02 10:53:35 -0500

    Added a bit more I guess

--- Tidy Data.R
+++ Tidy Data.R
@@ -19,6 +19,8 @@ successful_kickstarters <- kickstarter %>%
 #Filter full dataset down to the columns for goal, pledged, state, number of backers, and launch date
 #The units for goal and pledged are in US dollars and properly converted based on the static_usd_rate column
 reduced_dataset <- kickstarter %>%
-  select(goal, pledged, state, static_usd_rate, backers_count, launched_at_weekday) %>%
   mutate(goal = goal*static_usd_rate) %>%
-  mutate(pledged = pledged*static_usd_rate)
+  mutate(pledged = pledged*static_usd_rate) %>%
+  select(goal, pledged, state, static_usd_rate, backers_count, launched_at_weekday)
+  
+write.csv(reduced_dataset, "./reduced_dataset.csv", row.names = FALSE) #Create this and store on your local file system but don't commit this to github please.
