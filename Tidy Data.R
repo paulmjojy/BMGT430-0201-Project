766c25f44737685e9d81df7c8d61f91c45a685e7
minersail <jkwoo1234@gmail.com>
2019-12-01 22:49:27 -0500

    improve model

--- Tidy Data.R
+++ Tidy Data.R
@@ -1,4 +1,5 @@
 library(dplyr)
+library(car)
 
 ###Tidying up the data
 
@@ -19,11 +20,15 @@ successful_kickstarters <- kickstarter %>%
 #Filter full dataset down to the columns for goal, pledged, state, number of backers, and launch date
 #The units for goal and pledged are in US dollars and properly converted based on the static_usd_rate column
 reduced_dataset <- kickstarter %>%
-  select(goal, pledged, state, static_usd_rate, backers_count, launched_at_weekday) %>%
+  select(goal, pledged, state, static_usd_rate, backers_count, created_at_weekday, staff_pick, category, country, launch_to_deadline_days, create_to_launch_days) %>%
   mutate(goal = goal*static_usd_rate) %>%
   mutate(pledged = pledged*static_usd_rate)
 
 attach(reduced_dataset)
-launched_at_weekend <- launched_at_weekday == "Saturday" | launched_at_weekday == "Sunday"
-fit <- lm(log(pledged + 1)~log(goal + 1) + I(log(goal + 1)^2) + I(log(goal + 1)^3)+log(backers_count + 1)+launched_at_weekend)
+avglog_goal = log(goal + 1) - mean(log(goal + 1))
+avglog_backer = log(backers_count + 1) - mean(log(backers_count + 1))
+fit <- lm(log(pledged + 1)~avglog_goal + I(avglog_goal^2) + I(avglog_goal^3))
+vif(fit) # OK
+fit2 <- lm(log(pledged + 1)~avglog_goal + I(avglog_goal^2) + I(avglog_goal^3)+avglog_backer+I(avglog_backer^2)+staff_pick+launch_to_deadline_days+create_to_launch_days)
+vif(fit2) # Also OK
 summary(fit)
