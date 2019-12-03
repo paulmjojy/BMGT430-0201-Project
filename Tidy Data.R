ee29ca550d20ef56944fd2fb64781da01c6227c9
Anthony Sevilla <aympsh99@gmail.com>
2019-12-03 17:32:25 -0500

    log transform to fix LINE assumptions of logistic model

--- Tidy Data.R
+++ Tidy Data.R
@@ -13,40 +13,6 @@ kickstarter <- read.csv("./kickstarter_data_with_features.csv", header = TRUE, s
 
 #Conversion of Units
 #The units for goal and pledged are in US dollars and properly converted based on the static_usd_rate column
-<<<<<<< HEAD
-reduced_dataset <- kickstarter %>%
-  select(goal, pledged, state, static_usd_rate, backers_count, created_at_weekday, launched_at_weekday, staff_pick, category, country, launch_to_deadline, create_to_launch, launched_at_month, launched_at_day, launched_at_yr, deadline_month, deadline_day, deadline_yr) %>%
-  mutate(goal = goal*static_usd_rate) %>%
-  mutate(pledged = pledged*static_usd_rate) %>%
-  select(goal, pledged, state, static_usd_rate, backers_count, staff_pick, launch_to_deadline, create_to_launch,launched_at_weekday, launched_at_month, launched_at_day, launched_at_yr, deadline_month, deadline_day, deadline_yr)
-
-write.csv(reduced_dataset, "./reduced_dataset.csv", row.names = FALSE) #Create this and store on your local file system but don't commit this to github please.
-
-attach(reduced_dataset)
-#Paul and Anthony Section
-#This is to calculate the duration, in days, of the campaign
-deadline_date <- as.Date(with(reduced_dataset, paste(deadline_month, deadline_day, deadline_yr, sep="/")), format="%m/%d/%Y")
-launch_date <- as.Date(with(reduced_dataset, paste(launched_at_month, launched_at_day, launched_at_yr, sep="/")), format="%m/%d/%Y")
-reduced_dataset<-reduced_dataset[,-c(7:12)]
-reduced_dataset$duration<-deadline_date-launch_date
-
-#this is to turn launched_at_weekday into a binary variable. 1=weekday, 0 = weekend
-weekday <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
-weekend <- c("Saturday", "Sunday")
-reduced_dataset$launched_at_weekday[reduced_dataset$launched_at_weekday %in% weekday] <- 1
-reduced_dataset$launched_at_weekday[reduced_dataset$launched_at_weekday %in% weekend] <- 0
-
-#Convert campaign success into a binary variable, 1=success, 0 = fail
-logistic_data<-reduced_dataset[state != "live",]
-detach(reduced_dataset)
-attach(logistic_data)
-fail<- c("canceled","failed", "suspended")
-success<-c("successful")
-logistic_data$state[logistic_data$state %in% fail] <- 0
-logistic_data$state[logistic_data$state %in% success] <- 1
-logistic_data$state <- factor(state)
-logistic_data$launched_at_weekday <- factor(launched_at_weekday)
-=======
 kickstarter <- kickstarter %>%
   mutate(goal = goal*static_usd_rate) %>%
   mutate(pledged = pledged*static_usd_rate)
@@ -84,31 +50,17 @@ logistic_model$launched_at_weekday <- factor(logistic_model$launched_at_weekday)
 
 m<-glm(state~goal+launched_at_weekday+duration, data=logistic_model, family = binomial())
 summary(m)
+anova(m, test = "Chisq")
+#Test if the model is significant
+mred<-glm(state~1, data=logistic_model, family = binomial())
+anova(mred,m,test = "Chisq")
+#P-value was small so model is significant
+#Transformation to fix equal spread and linearity assumptions
+m1<=glm(state~I(log(goal))+launched_at_weekday+duration,data=logistic_model,family = binomial())
+summary(m1)
 
-
-
-##Anthonys Section
 detach(logistic_model)
-logistic_model_two <- kickstarter %>%  
-  select(goal, pledged, state, static_usd_rate, 
-         backers_count, staff_pick, launch_to_deadline, 
-         create_to_launch, launched_at_weekday, launched_at_month, 
-         launched_at_day, launched_at_yr, deadline_month, 
-         deadline_day, deadline_yr)
-#Remember that if you alter the number of rows or columns you'll need to reattach
-#Also if you're doubtful just do logistic_model_two$[Whatever this variable is]
-attach(logistic_model_two)
-
->>>>>>> 448845c6fc9599e52d1fff844b91cad1d05e9bdd
-
-m<-glm(state~goal+launched_at_weekday+duration, data=logistic_data, family = binomial())
-summary(m)
-
 ##Jordans Section
-<<<<<<< HEAD
-attach(reduced_dataset)
-detach(logistic_data)
-=======
 jordan_dataset <- kickstarter %>%
   mutate(goal = goal*static_usd_rate) %>%
   mutate(pledged = pledged*static_usd_rate) %>%
@@ -118,7 +70,7 @@ jordan_dataset <- kickstarter %>%
 
 attach(jordan_dataset)
 
->>>>>>> 448845c6fc9599e52d1fff844b91cad1d05e9bdd
+
 avglog_goal = log(goal + 1) - mean(log(goal + 1))
 avglog_backer = log(backers_count + 1) - mean(log(backers_count + 1))
 fit <- lm(log(pledged + 1)~avglog_goal + I(avglog_goal^2) + I(avglog_goal^3))
@@ -126,4 +78,3 @@ vif(fit) # OK
 fit2 <- lm(log(pledged + 1)~avglog_goal + I(avglog_goal^2) + I(avglog_goal^3)+avglog_backer+I(avglog_backer^2)+staff_pick+launch_to_deadline+create_to_launch)
 vif(fit2) # Also OK
 summary(fit)
-summary(fit2)
